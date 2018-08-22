e9ecd31e64be56a3c3bb20a8534f45268a300ed1
DavidVarga1995 <varga95david@gmail.com>
2018-08-22 16:16:49 +0200

    MasterDetailPage added, navigation changed

--- BLEACR/BLEACR/Pages/MainPage.xaml.cs
+++ BLEACR/BLEACR/Pages/MainPage.xaml.cs
@@ -1,13 +1,26 @@
-﻿using Xamarin.Forms;
+﻿using BLEACR.MasterPagePageltem;
+using System;
+using Xamarin.Forms;
 
 namespace BLEACR
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            masterPage.listView.ItemSelected += OnItemSelected;
         }
 
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is MasterDetailPageNavigation item)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
     }
 }
